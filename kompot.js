7f93c41e54baaeefe3338514667bd17464040248
Nir Yosef <niryo@wix.com>
2018-06-22 20:33:40 +0300

    kompot.require now returns a fancy builder

--- kompot.js
+++ kompot.js
@@ -1,43 +1,47 @@
-module.exports = {
-  require: function (getComponent, mocks={}) {
-    if (global.KompotApp) {
-      Object.keys(mocks).forEach(key => {
-        if(global[key]) {
-          mocks[key]();
-        }
-      });
-      global.KompotApp(getComponent())
-    }
-  },
- 
-  testComponent: function(name) {
-    const fetch = require('node-fetch');
-    const requests = [];
-    requests.push(fetch(`http://localhost:2600/setCurrentComponent?componentName=${name}`));
+const path = require('path');
 
-    return {
-      withMocks: function(globals) {
-        const query = globals.map(global => `${global}=true`).join('&');
-        requests.push(fetch(`http://localhost:2600/setGlobals?${query}`));
-        return this;
-      },
-      withProps: function(props) {
-        const query = Object.keys(props)
-        .map(key => {
-          let value = props[key];
-          if(isFunction(props[key])){
-            value = 'FUNCTION#'+ value.toString();
-          }
-          return `${key}=${encodeURIComponent(value)}`;
-        })
-        .join('&');
-        requests.push(fetch(`http://localhost:2600/setProps?${query}`));
-        return this;
-      },
-      mount : async function(){
-        return Promise.all(requests);
+module.exports = {
+  require: function (pathToComponent) {
+    const fetch = require('node-fetch');
+    const fileName = path.basename(pathToComponent, '.js');
+    const requests = [];
+    requests.push(fetch(`http://localhost:2600/setCurrentComponent?componentName=${fileName}`));
+
+    const testComponentBuilder =  {
+        withMocks: function(globals) {
+          const query = globals.map(global => `${global}=true`).join('&');
+          requests.push(fetch(`http://localhost:2600/setGlobals?${query}`));
+          return this;
+        },
+        withProps: function(props) {
+          const query = Object.keys(props)
+          .map(key => {
+            let value = props[key];
+            if(isFunction(props[key])){
+              value = 'FUNCTION#'+ value.toString();
+            }
+            return `${key}=${encodeURIComponent(value)}`;
+          })
+          .join('&');
+          requests.push(fetch(`http://localhost:2600/setProps?${query}`));
+          return this;
+        },
+        mount : async function(){
+          return Promise.all(requests);
+        }
+      };
+
+    const handler = {
+      get: (target,prop) => {
+        console.log('getting prop', prop)
+        if(testComponentBuilder.hasOwnProperty(prop)){
+          return testComponentBuilder[prop];
+        } else{
+          return testComponentBuilder;
+        }
       }
-    };
+    }
+    return new Proxy(testComponentBuilder, handler);
   }
 };
 
--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -1,3 +1,4 @@
+const _ = require('lodash'); 
 const execSync = require('child_process').execSync;
 const path = require('path');
 const fs = require('fs');
@@ -36,7 +37,9 @@ const requireStatements = filePathList
   .map(filePath => {
     const kompotRequire = readFileAndExtractKompotRquirePath(filePath);
     const fileName = path.basename(filePath, KOMPOT_FILE_EXTENTION);
-    return `if(global['${fileName}']){require('${kompotRequire}');}`;
+    return `if(global['${fileName}']){
+      currentComponent = require('${kompotRequire.path}')${kompotRequire.member? `.${kompotRequire.member}`: ''};
+    }`;
   }).join('\n');
 
 
@@ -50,7 +53,9 @@ if (args.app_type === 'react-native-navigation') {
 }
 const requireStatementsFunction = `
 export default function(){
+  let currentComponent;
   ${requireStatements}
+  global.KompotApp(currentComponent);
 }`;
 output = [registerRootComponent, requireStatementsFunction].join('\n');
 
@@ -65,17 +70,27 @@ function readFileAndExtractKompotRquirePath(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8');
   const ast = babylon.parse(content, {sourceType: 'module'});
   let kompotRequireAbsolutePath;
+  let kompotRequireMember;
   traverse(ast, {
     CallExpression: ({node}) => {
-      if(node.callee.object && node.callee.object.name === 'Kompot' && node.callee.property.name === 'require') {
+      const objectName = _.get(node, 'callee.object.name');
+      const methodName = _.get(node, 'callee.property.name');
+      if(objectName === 'Kompot' && methodName === 'require') {
         const kompotRequireRelativePath = node.arguments[0].extra.rawValue;
-        kompotRequireAbsolutePath = path.resolve(filePath,kompotRequireRelativePath);
+        kompotRequireAbsolutePath = path.resolve(path.dirname(filePath),kompotRequireRelativePath);
+      }
+    },
+    MemberExpression: ({node}) => {
+      const objectName = _.get(node, 'object.callee.object.name');
+      const methodName = _.get(node, 'object.callee.property.name');
+      if(objectName === 'Kompot' && methodName === 'require') {
+        kompotRequireMember = node.property.name;
       }
     }
   });
   if(kompotRequireAbsolutePath){
     console.log('Found kompot require statement:', kompotRequireAbsolutePath, ' in file: ', filePath);
-    return kompotRequireAbsolutePath;
+    return {path: kompotRequireAbsolutePath, member: kompotRequireMember};
   } else {
     throw new Error('Cannot find kompot require statement')
   }
