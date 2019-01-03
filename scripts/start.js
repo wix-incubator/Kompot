09393fc6a38c69ec899f759a93c5b2e26fcd8b73
Nir Yosef <niryo@wix.com>
2019-01-03 13:07:07 +0200

    support rn57

--- scripts/start.js
+++ scripts/start.js
@@ -1,2 +1,2 @@
 let spawn = require('child_process').spawn;
-spawn('react-native', ['start', '--projectRoots',`${__dirname}/../,${__dirname}/../../../`], { stdio: 'inherit' });
+spawn('react-native', ['start', '--projectRoot',`${__dirname}/../`], { stdio: 'inherit' });
