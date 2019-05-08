ae2babca3b41ecf8e5b9dd216a9e4720dfcff105
N Y <niryosef89@gmail.com>
2019-05-08 15:56:15 +0300

    support rn59

--- scripts/start.js
+++ scripts/start.js
@@ -1,2 +1,2 @@
 let spawn = require('child_process').spawn;
-spawn('react-native', ['start', '--projectRoot',`${__dirname}/../`], { stdio: 'inherit' });
+spawn('./node_modules/.bin/react-native', ['start', '--config', './node_modules/kompot/metro.config.js'], { stdio: 'inherit' });
