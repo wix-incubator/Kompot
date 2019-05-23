86a77dbb96ef80a07275d7c56aacbdf38b94b398
N Y <niryosef89@gmail.com>
2019-05-23 20:12:18 +0300

    spies fixes

--- initKompotTestSuite.js
+++ initKompotTestSuite.js
@@ -69,11 +69,14 @@ function spyHandlers(spy) {
               foundCallWithMatchedArgs = true;
             }
           } catch (e) {
+            console.log(e);
           }
         });
         if (!foundCallWithMatchedArgs) {
           throw Error(`Expected spy with id "${spy.id}" to have been called with:\n\n ${args}\n But it was called with:\n${calls}`);
         }
+      } else {
+          throw Error(`Expected spy with id "${spy.id}" to have been called with:\n\n ${args}\n But it was never called`);
       }
     },
 
@@ -83,13 +86,15 @@ function spyHandlers(spy) {
         throw Error(`Expected spy with id "${spy.id}" to be called ${callNum} times, but it was called only ${calls.length} times`);
       }
       if (calls) {
+        let parsedCall;
         try{
-          const parsedCall = JSON.parse(calls[callNum]);
-          if (!isEqual(parsedCall, args)) {
-            throw Error(`Expected spy with id "${spy.id}" call number ${callNum} to be:\n\n ${args}\n But it was:\n${parsedCall}`);
-          }
+          parsedCall = JSON.parse(calls[callNum]);
         } catch(e) {
-          
+          console.log(e);
+        }
+
+        if (!isEqual(parsedCall, args)) {
+          throw Error(`Expected spy with id "${spy.id}" call number ${callNum} to be:\n\n ${JSON.stringify(args)}\n But it was:\n${JSON.stringify(parsedCall)}`);
         }
       }
     }
