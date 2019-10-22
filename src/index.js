5b919d2ba32ba576bf0f50c89e8657376b413b92
N Y <niryosef89@gmail.com>
2019-10-22 12:34:59 +0300

    remove mocking leftovers

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import React from 'react';
 import {deSerialize} from './Serialize';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
-//========= Mock Object.defineProperty to always allow overriding =========
-const originalDefineProperty = Object.defineProperty;
-const originalDefineProperties = Object.defineProperties;
-Object.defineProperty = (obj, prop, desc) => {
-  return originalDefineProperty(obj, prop, {...desc, configurable: true});
-};
-Object.defineProperties = (obj, props) => {
-  Object.keys(props).forEach((key) => {
-    props[key].configurable = true;
-  });
-  return originalDefineProperties(obj, props);
-};
-//=========================================================================
-
 const originalFetch = fetch;
 const providers = [];
 const mockedUrls = {};
