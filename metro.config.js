ee0c54f26bff5e25d7fc6cfb993287bbc579906a
N Y <niryosef89@gmail.com>
2019-12-19 18:41:01 +0200

    rn61 support

--- metro.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  watchFolders: [`${__dirname}/../..`],
-};
--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,2 +1,2 @@
 let spawn = require('child_process').spawn;
-spawn('./node_modules/.bin/react-native', ['start', '--projectRoot',`${__dirname}/../`], { stdio: 'inherit' });
+spawn('./node_modules/.bin/react-native', ['start', '--projectRoot',`${__dirname}/../`, '--watchFolders', `${__dirname}/../../../../`], { stdio: 'inherit' });
