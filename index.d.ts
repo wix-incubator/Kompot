9b042ad095c8bb0fa4443c37a28349263ba16108
Dor256 <dorb@wix.com>
2020-06-04 16:50:09 +0300

    Adds initial typings

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+type TestComponent = {
+  withMocks(mocks: any[]): TestComponent;
+  withProps(props: {[key: string]: any}): TestComponent;
+  withTriggers(triggers: any[]): TestComponent;
+  mount(): Promise<void>;
+};
+
+export = kompot;
+export as namespace kompot;
+
+declare namespace kompot {
+  function spy(id: string, getReturnValue?: (args: any[]) => any, stringifyArgs?: (args: any[]) => string[]): any
+  function kompotRequire(pathToComponent: string): {[key: string]: TestComponent};
+  function init(): void;
+}
