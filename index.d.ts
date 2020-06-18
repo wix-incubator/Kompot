7a045e247b501f8de8e479d91fa32de69da9e600
Dor256 <dorb@wix.com>
2020-06-18 22:02:24 +0300

    PR Fixes

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
+type FunctionArray = Array<() => any>;
+
 type TestComponent = {
-  withMocks(mocks: any[]): TestComponent;
+  withMocks(mocks: FunctionArray): TestComponent;
   withProps(props: { [key: string]: any }): TestComponent;
-  withTriggers(triggers: any[]): TestComponent;
+  withTriggers(triggers: FunctionArray): TestComponent;
   mount(): Promise<void>;
 };
 
@@ -12,5 +14,5 @@ declare namespace kompot {
   function spy(id: string, getReturnValue?: (args: any[]) => any, stringifyArgs?: (args: any[]) => string[]): any
   function kompotRequire(pathToComponent: string): { [key: string]: TestComponent };
   function init(): void;
-  function expect<T>(value: T): jest.Matchers<T> & Detox.Expect<T>
+  function expect<T>(value: T): jest.Matchers<T> & Detox.Expect<T> & {notToHaveBeenCalled(): Promise<void>}
 }
